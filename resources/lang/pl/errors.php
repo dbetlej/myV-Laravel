e978b5d11d5d65b24ac883f8e88538b5a4420e6c
dbetlej <dominik.filip.betlej@gmail.com>
2022-02-07 14:45:20 +0100

    fix translation 404 [pl]

--- resources/lang/pl/errors.php
+++ resources/lang/pl/errors.php
@@ -2,7 +2,7 @@
 
 return [
     'cupid_name' => 'Proszę podać nazwę.',
-    '404' => 'STRONA NIE ZNALEZIONA',
+    '404' => 'STRONA NIEZNALEZIONA',
     'back404' => 'powrót do strony głównej',
     'content' => 'Brak życzeń',
 ];
